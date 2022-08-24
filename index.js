0fa1ce24995d23152129d39ad0582183be5209a7
Martin Stone <martindstone@me.com>
2022-08-24 12:51:47 -0400

    initial+1

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function main() {
   const el = document.querySelector('#content');
   const params = new URLSearchParams(location.search);
-  el.innerHTML = '';
-  for (const [k, v] of params.entries()) {
-    el.innerHTML += `${k}: ${v}<br>`;
+  const config = JSON.parse(params.get('config'));
+
+  const subdomain = config.account.subdomain;
+  el.innerHTML = '<h2>great. Configured on these services:</h2><ul>'
+  for (key of config.integration_keys) {
+    el.innerHTML += `
+      <li><a href="https://${subdomain}.pagerduty.com/services/${key.id}">${key.name}</a> (routing key ${key.integration_key})
+    `;
   }
+  el.innerHTML += '</ul>'
 }
 
 (() => {
